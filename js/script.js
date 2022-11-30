f172ba7161bc22eacc12729d34a7cea3887af1bd
romanjhyltsou <romanjhyltsou@gmail.com>
2022-12-01 01:20:33 +0300

    refactor:restored old code

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const ctx = canvas.getContext('2d'); //default width 300px height 150px
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
 let gameSpeed = 5;
-let gameFrame = 0;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = 'assets/img/layer-1.png';
@@ -33,17 +32,25 @@ class Layer {
         this.y = 0;
         this.width = 2400;
         this.height = 700;
+       /*  this.x2 = this.width; */
         this.image = image;
         this.speedModifier = speedModifier;
         this.speed = gameSpeed * this.speedModifier;
     }
     update(){
         this.speed = gameSpeed * this.speedModifier;
-        this.x = gameFrame * this.speed % this.width;
+        if(this.x <= -this.width){
+           this.x = 0/* this.width + this.x2 - this.speed */; 
+        }
+/*         if(this.x2 <= -this.width){
+            this.x2 = this.width + this.x - this.speed; 
+        } */
+        this.x = Math.floor(this.x - this.speed);
+       /*  this.x2 = Math.floor(this.x2 - this.speed); */
     }
     draw(){
       ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-      ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+      ctx.drawImage(this.image, this.x + this.width/* this.x2 */, this.y, this.width, this.height);
     }
 }
 
@@ -69,7 +76,6 @@ function animate() {
   else x -= gameSpeed;
   if(x2 < -2400) x2 = 2400 + x - gameSpeed; 
   else x2 -= gameSpeed; */
-  gameFrame--;
   requestAnimationFrame(animate);
   
 }
